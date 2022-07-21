a661326f292a69e8551d4ed4065ae85342f4c052
babala <hua19930930@gmail.com>
2022-07-21 18:07:59 +0800

    study Node

---
+++ src/main/java/part_one/FIFOQueue.java
@@ -0,0 +1,40 @@
+package part_one;
+
+public class FIFOQueue<Item> {
+    private Node first; // link to least recently added node
+    private Node last; // link to most recently added node
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void enqueue(Item item) {
+        //add item to the end if the list
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldlast.next = last;
+        N++;
+    }
+
+    public Item dequeue() {
+        // remove item from beginning of the list
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) last = null;
+        N--;
+        return item;
+    }
+}
---
+++ src/main/java/part_one/LIFOStack.java
@@ -0,0 +1,44 @@
+package part_one;
+
+public class LIFOStack<Item> {
+
+    private Node first;
+    private int N;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        N++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        N--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        LIFOStack lifoStack = new LIFOStack<String>();
+
+        lifoStack.push("name");
+        lifoStack.push("age");
+        lifoStack.pop();
+    }
+
+}
